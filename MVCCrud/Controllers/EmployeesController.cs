002471bec60f6474634f574ad2fd2b1f81b06821
Akit kumar <ankitjh521@gmail.com>
2020-03-26 21:12:59 +0530

    mvc crud implemented

---
+++ MVCCrud/Controllers/EmployeesController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Models;
+
+namespace MVCCrud.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        public static IList<Employee> employees = new List<Employee>()
+        {
+            new Employee{Id = 1, Name="Ankit", Desgination="Trainee", Salary=12000},
+
+            new Employee{Id = 2, Name="Ram", Desgination="Trainee", Salary=12000},
+            
+            new Employee{Id = 3, Name="Shyam", Desgination="Trainee", Salary=12000},
+            
+            new Employee{Id = 4, Name="Ramlal", Desgination="Trainee", Salary=12000}
+        };
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Partial View for Employee GRID
+        public ActionResult EmployeeGrid()
+        {
+            return PartialView(employees);
+        }
+
+        public ActionResult EmployeeModal(int id)
+        {
+            if(id == 0)
+            {
+                Employee employee = new Employee()
+                {
+                    Id = employees.Count+1,
+                    Name = "",
+                    Desgination = "",
+                    Salary = 0
+                };
+               
+                return PartialView(employee);
+            }
+            else
+            {
+                var employee = employees.FirstOrDefault(e => e.Id == id);
+                return PartialView(employee);
+            }
+            
+        }
+
+        [HttpPost]
+        public bool UpdateEmployee(Employee employee)
+        {
+            var newemp = employees.FirstOrDefault(emp => emp.Id == employee.Id);
+            if (newemp != null)
+            {
+                newemp.Id = employee.Id;
+                newemp.Name = employee.Name;
+                newemp.Desgination = employee.Desgination;
+                newemp.Salary = employee.Salary;
+
+                employees.Where(e => e.Id == employee.Id).ToList().ForEach(e =>
+                {
+                    e.Id = newemp.Id;
+                    e.Name = newemp.Name;
+                    e.Desgination = newemp.Desgination;
+                    e.Salary = newemp.Salary;
+                });
+                return true;
+            }
+            else
+            {
+                employees.Add(employee);
+                return true;
+            }
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            if(employees.Contains(employees.Where(emp => emp.Id == id).First()))
+            {
+                employees.Remove(employees.Where(emp => emp.Id == id).First());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+           
+        }
+    }
+}
---
+++ MVCCrud/Scripts/Employee.js
@@ -0,0 +1,132 @@
+﻿
+
+$(function () {
+    displayGrid();
+});
+
+function displayGrid() {
+
+    $.ajax({
+        type: "Get",
+        url: '/Employees/EmployeeGrid',
+        //data: '{customerId: "' + customerId + '" }',
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        success: function (response) {
+            $('#employeegrid').html(response);
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
+
+function loadModal(id) {
+    console.log('id', id);
+    $.ajax({
+        type: "Get",
+        url: '/Employees/EmployeeModal?id=' + id,
+        //data: id,
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        success: function (response) {
+            console.log(response);
+            $('#bodyMain').html(response);
+            $('#modalHead').show();
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
+
+function closeModal() {
+    $('#modalHead').hide();
+}
+
+function saveemployee() {
+    let employee = {
+        Id: document.getElementById('id').value,
+        Name: document.getElementById('name').value,
+        Desgination: document.getElementById('designation').value,
+        Salary: document.getElementById('salary').value
+    }
+
+    //let formdata = new FormData($('#employeeform').get(0));
+
+    $.ajax({
+        type: "POST",
+        url: '/Employees/UpdateEmployee',
+        data: JSON.stringify(employee),
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        success: function (response) {
+            if (response) {
+                closeModal();
+                alert('Details Updated');
+                displayGrid();
+            }
+            
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
+
+
+function deleteEmployee(id) {
+    $.ajax({
+        type: "Get",
+        url: '/Employees/DeleteEmployee?id='+id,
+        //data: JSON.stringify(employee),
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        success: function (response) {
+            if (response) {
+                alert('Employee record deleted');
+                displayGrid();
+            }
+            
+            //$('#employeegrid').html(response);
+            //location.reload();
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
+//document.onload = displayGrid();
+
+function addEmployee(id) {
+    $.ajax({
+        type: "Get",
+        url: '/Employees/EmployeeModal?id=' + id,
+        //data: id,
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        success: function (response) {
+            console.log(response);
+            $('#bodyMain').html(response);
+            $('#modalHead').show();
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
